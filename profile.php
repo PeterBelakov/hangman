620c28eeb9f31f3e2d8e12e1610ad446cc338228
PeterBelakov <p.belakov89@gmail.com>
2017-01-12 17:25:03 +0200

    Fixed bugs in word.

--- profile.php
+++ profile.php
@@ -7,14 +7,17 @@ include 'application.php';
 
 if (isset($_SESSION['user'])) {
     $username = $_SESSION['user'];
-
+    // recording a user_id in session
     $data = $userLifecycle->getUserInfo($username);
     $_SESSION['user_id'] = intval($data->getId());
     $user_id = $_SESSION['user_id'];
 
+    //making all categories DB
     $category = $wordProcessing->getAllCategory();
     $waste = 0;
     $victory = 0;
+
+    //making statistic user DB
     if ($userStatistics = $statistics->getStatisticInfo($user_id)) {
 
         $userStatistics = $statistics->getStatisticInfo($user_id);
@@ -31,8 +34,6 @@ if (isset($_SESSION['user'])) {
 
     $wordName = null;
     $max = 6;
-
-
     if (isset($_POST['category'])) {
         #New game started
 
@@ -41,6 +42,7 @@ if (isset($_SESSION['user'])) {
         $_SESSION['answer'] = [];
 
         $category_id = intval($_POST['category']);
+        // making random word database
         $wordName = $wordProcessing->getWord($category_id);
         $_SESSION['category'] = $category_id;
         $_SESSION['wordLength'] = (strlen($wordName) - 2);
@@ -49,41 +51,46 @@ if (isset($_SESSION['user'])) {
         $_SESSION['error'] = 0;
         $_SESSION['successful_letters_int'] = 0;
     }
+    //
     if (isset($_GET['letter']) && isset($_SESSION['selected_letter'])) {
-
         if (!in_array($_GET['letter'], $_SESSION['selected_letter'])) {
-            $_SESSION['selected_letter'][] = $_GET['letter'];
-        }
-        if (in_array($_GET['letter'], $_SESSION['answer']) && (current($_SESSION['answer']) != $_GET['letter']) && (end($_SESSION['answer']) != $_GET['letter'])) {
 
-            $_SESSION['successful_letters_int'] += 1;
-        } elseif (current($_SESSION['answer']) == $_GET['letter'] || (end($_SESSION['answer']) == $_GET['letter'])) {
-            $_SESSION['successful_letter'][] = $_GET['letter'];
-        } else {
-            $_SESSION['error'] += 1;
-        }
-        if ($_SESSION['error'] >= $max) {
-            $_SESSION['answer'] = [];
-            echo 'GAME OVER!';
-            $waste += 1;
+            if (!in_array($_GET['letter'], $_SESSION['selected_letter'])) {
+                $_SESSION['selected_letter'][] = $_GET['letter'];
+
+            }
+            if (in_array($_GET['letter'], $_SESSION['answer']) && (current($_SESSION['answer']) != $_GET['letter']) && (end($_SESSION['answer']) != $_GET['letter'])) {
+                $answerCount = array_count_values($_SESSION['answer']);
+                $answerCount[$_GET['letter']];
+                $_SESSION['successful_letters_int'] += $answerCount[$_GET['letter']];   
+            } elseif (current($_SESSION['answer']) == $_GET['letter'] || (end($_SESSION['answer']) == $_GET['letter'])) {
+                $_SESSION['successful_letter'][] = $_GET['letter'];
+            } else {
+                $_SESSION['error'] += 1;
+            }
+            if ($_SESSION['error'] >= $max) {
+                $_SESSION['answer'] = [];
+                echo 'GAME OVER!';
+                $waste += 1;
+
+            }
+            if ($_SESSION['successful_letters_int'] === $_SESSION['wordLength']) {
+                echo 'CONGRATULATIONS YOU GUESSED IT!';
+                $victory += 1;
+
+            }
+
+            $statisticData = [];
+            $statisticData['victory_game'] = $victory;
+            $statisticData['waste_game'] = $waste;
+            $statisticData['user_id'] = $_SESSION['user_id'];
+            $statistics->updateStatistic($statisticData);
 
         }
-        if ($_SESSION['successful_letters_int'] === $_SESSION['wordLength']) {
-            echo 'CONGRATULATIONS YOU GUESSED IT!';
-            $victory += 1;
-        }
-
-        $statisticData = [];
-        $statisticData['victory_game'] = $victory;
-        $statisticData['waste_game'] = $waste;
-        $statisticData['user_id'] = $_SESSION['user_id'];
-        $statistics->updateStatistic($statisticData);
     }
-    echo "Victory:" . $victory;
+    echo "Victory:" . $victory . '</br>';
     echo "Waste:" . $waste;
     echo '</br>';
-
-
     if (!empty($_SESSION['wordLength'])) {
         //$_SESSION['hiddenWord'] = array_fill(0, $_SESSION['wordLength'], '_');
 
